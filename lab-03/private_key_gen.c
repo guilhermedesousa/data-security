3275726220946a0a0588a5a8b73b94c0892e0d42
Guilherme Santos <44320884+guilhermedesousa@users.noreply.github.com>
2024-03-17 00:13:06 +0000

    code the RSA encryption

--- lab-03/private_key_gen.c
+++ lab-03/private_key_gen.c
@@ -5,8 +5,6 @@
 
 void printBN(char *msg, BIGNUM * a)
 {
-    // Use BN_bn2hex(a) for hex string
-    // Use BN_bn2dec(a) for decimal string
     char * number_str = BN_bn2hex(a);
     printf("%s %s\n", msg, number_str);
     OPENSSL_free(number_str);
@@ -14,46 +12,57 @@ void printBN(char *msg, BIGNUM * a)
 
 int main ()
 {
-    // temporary variable
-    BN_CTX *ctx = BN_CTX_new();
+    BIGNUM *p, *q, *e, *n, *z, *d, *check;
+    BN_CTX *ctx = BN_CTX_new(); // temporary variable
 
-    BIGNUM *p = BN_new();
-    BIGNUM *q = BN_new();
-    BIGNUM *e = BN_new();
-    BIGNUM *n = BN_new();
-    BIGNUM *z = BN_new();
-    BIGNUM *d = BN_new();
+    // initialize the big numbers
+    p = BN_new();
+    q = BN_new();
+    e = BN_new();
+    n = BN_new();
+    z = BN_new();
+    d = BN_new();
+    check = BN_new();
 
-    // initialize p, q, e
+    // define p, q, e
     BN_hex2bn(&p, "F7E75FDC469067FFDC4E847C51F452DF");
     BN_hex2bn(&q, "E85CED54AF57E53E092113E62F436F4F");
     BN_hex2bn(&e, "0D88C3");
 
-    // calculating n
+    // calculate n = p * q
     BN_mul(n, p, q, ctx);
 
-    BIGNUM *sub_p = BN_new();
-    BIGNUM *sub_q = BN_new();
-    BIGNUM *i = BN_new();
-    BN_dec2bn(&i, "1");
-    
-    // calculating (p-1)
-    BN_sub(sub_p, p, i);
-
-    // calculating (q-1)
-    BN_sub(sub_q, q, i);
-
-    // calculating z
-    BN_mul(z, sub_p, sub_q, ctx);
+    // calculate z = (p-1) * (q-1)
+    BN_sub_word(p, 1);
+    BN_sub_word(p, 1);
+    BN_mul(z, p, q, ctx);
 
     printBN("n = ", n);
     printBN("z = ", z);
 
-    // calculating d
+    // calculate d = e^(-1) mod z
     BN_mod_inverse(d, e, z, ctx);
 
     printBN("d = ", d);
-    printBN("i = ", i);
+
+    // verify if e * d mod z = 1
+    BN_mod_mul(check, e, d, z, ctx);
+    
+    if (BN_cmp(check, BN_value_one()) == 0) {
+        printf("d is correct.\n");
+    } else {
+        printf("d is incorrect.\n");
+    }
+
+    // free memory
+    BN_free(p);
+    BN_free(q);
+    BN_free(e);
+    BN_free(n);
+    BN_free(z);
+    BN_free(d);
+    BN_free(check);
+    BN_CTX_free(ctx);
     
     return 0;
 }
